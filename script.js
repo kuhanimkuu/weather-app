d5067b5c753e4e4438a714f34a415d8dd2215a98
kuhanimkuu <davidmurunga@gmail.com>
2025-05-01 17:53:53 +0300

    fourth commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function getWeather(e){
     const apiKey = "a7f799ab2b02523e67c3bc7e0540c305"
    
     const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`)
-
+    
     const response = await request.json()
     const city = document.getElementById("city")
     const humidity=document.getElementById("humidity")
@@ -19,9 +19,15 @@ async function getWeather(e){
     const temp_min = document.getElementById("temp_min")
     const pressure = document.getElementById("pressure")
     const speed = document.getElementById("speed")
+    const displays =document.getElementById("display-elements")
     city.textContent=location;
-    
-
+    console.log(response.name)
+    if(response.name!= location){
+        const invalid = document.createElement("div")
+        invalid.textContent=`doesn't exist in our database try again`
+        displays.appendChild(invalid)
+        
+    }
     humidity.textContent =`Humidity: ${response.main.humidity}`
     feel.textContent = `How it feels: ${response.main.feels_like} C`;
     temp.textContent=`Temp: ${response.main.temp} C`;
