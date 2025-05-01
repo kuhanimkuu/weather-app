223ab4969e84104b487ca4ccf9d793298ba554e3
kuhanimkuu <davidmurunga@gmail.com>
2025-05-01 17:47:34 +0300

    third commit

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 
 // Get location from user
-document.getElementById("submit").addEventListener("click",async function(e){
-    e.preventDefault()
+async function getWeather(e){
+
     const display = document.getElementById("display")
     const location = document.getElementById("location").value;
-    // console.log(location)
+    const loader = document.getElementById("loader")
+    loader.style.display = "block"
     const apiKey = "a7f799ab2b02523e67c3bc7e0540c305"
-    // await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-    // .then(response => response.json())
-    // console.log(response)
+   
     const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`)
+
     const response = await request.json()
-   const zero = 0
-    const weather = response.weather
-    console.log(weather)
     const city = document.getElementById("city")
     const humidity=document.getElementById("humidity")
     const feel = document.getElementById("feel")
@@ -23,6 +20,8 @@ document.getElementById("submit").addEventListener("click",async function(e){
     const pressure = document.getElementById("pressure")
     const speed = document.getElementById("speed")
     city.textContent=location;
+    
+
     humidity.textContent =`Humidity: ${response.main.humidity}`
     feel.textContent = `How it feels: ${response.main.feels_like} C`;
     temp.textContent=`Temp: ${response.main.temp} C`;
@@ -30,4 +29,12 @@ document.getElementById("submit").addEventListener("click",async function(e){
     temp_min.textContent=`Min-temp: ${response.main.temp_min} C`;
     pressure.textContent=`Pressure: ${response.main.pressure} bars`;
     speed.textContent=`Wind-speed: ${response.wind.speed} m/s`;
+}
+document.getElementById("submit").addEventListener("click",(e)=>{
+    e.preventDefault()
+    getWeather()
+    .catch(error => console.log('Catch error'+ error.message))
+    .finally(()=>{
+        loader.style.display = "none";
+    })
 })
